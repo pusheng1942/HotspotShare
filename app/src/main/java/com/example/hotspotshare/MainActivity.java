09e4d528adc5c47fa12fa65486b0d26c011b7b4a
pugong <pu.gong@intel.com>
2019-07-15 20:28:51 +0800

    Slove the flashback problem

--- app/src/main/java/com/example/hotspotshare/MainActivity.java
+++ app/src/main/java/com/example/hotspotshare/MainActivity.java
@@ -1,16 +1,32 @@
 package com.example.hotspotshare;
-
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
-import android.widget.TabHost;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     TextView wifiDisplayView;
@@ -18,23 +34,31 @@ public class MainActivity extends AppCompatActivity {
     Switch wifiSwitch;
     WifiHotspotConfig wifiHotspotConfig = new WifiHotspotConfig(this);
     Button updateButton;
-    String wifiSSID;
+
+    public static int AP_STATE_DISABLING = 10;
+    public static int AP_STATE_DISABLED = 11;
+    public static int AP_STATE_ENABLING = 12;
+    public static int AP_STATE_ENABLED = 13;
+    public static int AP_STATE_FAILED = 14;
+    public static String SSID;
+    public static String preShareKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         initWifiHotspotDisplay();
         updateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(wifiHotspotConfig.isHotSpotEnabled()){
+                if(isHotSpotEnabled()){
                     wifiStateView.setText("HotspotState:Open");
-                    wifiDisplayView.setText("SSID:"+wifiHotspotConfig.SSID+"\n\n"+"PWD:"+wifiHotspotConfig.preShareKey);
+                    wifiDisplayView.setText("SSID:"+SSID+"\n\n"+"PWD:"+preShareKey);
                 }
                 else {
                     wifiStateView.setText("HotspotState:Closed");
-//                    wifiDisplayView.setText("Not Found");
+                    wifiDisplayView.setText("Not Found");
                 }
             }
         });
@@ -43,10 +67,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
+//                    wifiHotspotConfig.closeHotSpot();
+                    turnOnHotspot();
                     wifiStateView.setText("HotspotState:Open");
-                    wifiHotspotConfig.turnOnHotspot();
                 }
                 else{
+                    turnOffHotspot();
+//                    wifiHotspotConfig.closeHotSpot();
                     wifiStateView.setText("HotspotState:Close");
                 }
             }
@@ -72,15 +99,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume(){
         super.onResume();
-//        wifiHotspotConfig.turnOnHotspot(this);
+//        wifiHotspotConfig.turnOnHotspot();
     }
 
     @Override
     protected void onStart(){
         super.onStart();
-        wifiSSID = wifiHotspotConfig.getWIFISSID(this);
-        wifiHotspotConfig.configureHotspot("ACD","432534654");
-//        wifiHotspotConfig.turnOnHotspot(this);
     }
 
     @Override
@@ -94,4 +118,162 @@ public class MainActivity extends AppCompatActivity {
         updateButton = findViewById(R.id.wifi_update);
         wifiSwitch = findViewById(R.id.wifi_switch);
     }
+
+    public boolean isHotSpotEnabled() {
+        Method method = null;
+        int actualState = 0;
+        try {
+            WifiManager mWifiManager = (WifiManager) this.getSystemService(WIFI_SERVICE);
+            method = mWifiManager.getClass().getDeclaredMethod("getWifiApState");
+            method.setAccessible(true);
+
+            actualState = (Integer) method.invoke(mWifiManager, (Object[]) null);
+            if (actualState == AP_STATE_ENABLING || actualState == AP_STATE_ENABLED) {
+                return true;
+            }
+        } catch (IllegalArgumentException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    private boolean isLocationPermissionEnable() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
+            return false;
+        }
+        return true;
+    }
+
+    private static WifiManager.LocalOnlyHotspotReservation mReservation;
+    private static boolean isHotspotEnabledState = false;
+
+    /**
+     * description:turnOnHotspot() just start the hotspot
+     * and can not setup the SSID and Password
+     *
+     * @param
+     * @param
+     * @return
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void turnOnHotspot() {
+        if (!isLocationPermissionEnable()) {
+            return;
+        }
+        WifiManager manager = (WifiManager) getSystemService(WIFI_SERVICE);
+
+        int wifiState = manager.getWifiState();
+        if ((wifiState == WifiManager.WIFI_STATE_ENABLING) || (wifiState == WifiManager.WIFI_STATE_ENABLED)) {
+            manager.setWifiEnabled(false);
+        }
+
+        if ((wifiState == WifiManager.WIFI_STATE_DISABLED)) {
+            manager.startLocalOnlyHotspot(new WifiManager.LocalOnlyHotspotCallback() {
+
+                @Override
+                public void onStarted(WifiManager.LocalOnlyHotspotReservation reservation) {
+                    super.onStarted(reservation);
+                    mReservation = reservation;
+                    SSID = reservation.getWifiConfiguration().SSID;
+                    preShareKey = reservation.getWifiConfiguration().preSharedKey;
+                    isHotspotEnabledState = true;
+                }
+
+                @Override
+                public void onStopped() {
+                    super.onStopped();
+                    isHotspotEnabledState = false;
+                }
+
+                @Override
+                public void onFailed(int reason) {
+                    super.onFailed(reason);
+                    isHotspotEnabledState = false;
+                }
+            }, new Handler());
+        }
+    }
+
+    public void turnOffHotspot() {
+        if (isHotspotEnabledState == true && mReservation != null) {
+            mReservation.close();
+            isHotspotEnabledState = false;
+        }
+    }
+
+    public ArrayList<String> getConnectedIP(){
+        ArrayList<String> connectedIp=new ArrayList<String>();
+        try {
+            BufferedReader br=new BufferedReader(new FileReader(
+                    "/proc/net/arp"));
+            String line;
+            while ((line=br.readLine())!=null){
+                String[] splitted=line.split(" +");
+                if (splitted !=null && splitted.length>=4){
+                    String ip=splitted[0];
+                    if (!ip.equalsIgnoreCase("ip")){
+                        connectedIp.add(ip);
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return connectedIp;
+    }
+
+    /**
+     * open the mobile network
+     * @param enabled  open or closed
+     */
+    public void setMobileDataState(boolean enabled) {
+        TelephonyManager telephonyService = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        try {
+            Method setDataEnabled = telephonyService.getClass().getDeclaredMethod("setDataEnabled",boolean.class);
+            if (null != setDataEnabled) {
+                setDataEnabled.invoke(telephonyService, enabled);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param on Boolean status
+     */
+    public boolean enableMobileData(boolean on) {
+        try {
+            ConnectivityManager mConnectivityManager =
+                    (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            Method method = mConnectivityManager.getClass().getMethod("setMobileDataEnabled", boolean.class);
+            method.invoke(mConnectivityManager, on);
+            return true;
+        } catch (IllegalArgumentException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public void closeHotSpot(){
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        Field iConnMgrField;
+        try {
+            iConnMgrField = connManager.getClass().getDeclaredField("mService");
+            iConnMgrField.setAccessible(true);
+            Object iConnMgr = iConnMgrField.get(connManager);
+            Class<?> iConnMgrClass = Class.forName(iConnMgr.getClass().getName());
+            Method stopTethering = iConnMgrClass.getMethod("stopTethering", int.class);
+            stopTethering.invoke(iConnMgr, 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
--- app/src/main/java/com/example/hotspotshare/WifiHotspotConfig.java
+++ app/src/main/java/com/example/hotspotshare/WifiHotspotConfig.java
@@ -5,19 +5,22 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.net.wifi.WifiConfiguration;
-import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Handler;
-import android.text.TextUtils;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 import static android.content.Context.WIFI_SERVICE;
 
@@ -33,14 +36,14 @@ public class WifiHotspotConfig {
     /**
      * @return status hot spot enabled or not
      */
-    Context context;
-    Activity activity;
-
-    public  WifiHotspotConfig(Context context){
-        this.context=context;
+    private Context context;
+    private Activity activity;
+//
+    public WifiHotspotConfig(Context context) {
+        this.context = context;
     }
 
-    public  boolean isHotSpotEnabled() {
+    public boolean isHotSpotEnabled() {
         Method method = null;
         int actualState = 0;
         try {
@@ -50,7 +53,6 @@ public class WifiHotspotConfig {
 
             actualState = (Integer) method.invoke(mWifiManager, (Object[]) null);
             if (actualState == AP_STATE_ENABLING || actualState == AP_STATE_ENABLED) {
-                getHotspotSSID(context);
                 return true;
             }
         } catch (IllegalArgumentException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
@@ -61,10 +63,118 @@ public class WifiHotspotConfig {
         return false;
     }
 
+    private boolean isLocationPermissionEnable(Activity activity) {
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
+            return false;
+        }
+        return true;
+    }
+
+    private static WifiManager.LocalOnlyHotspotReservation mReservation;
+    private static boolean isHotspotEnabledState = false;
+
+    /**
+     * description:turnOnHotspot() just start the hotspot
+     * and can not setup the SSID and Password
+     *
+     * @param
+     * @param
+     * @return
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void turnOnHotspot() {
+        if (!isLocationPermissionEnable(activity)) {
+            return;
+        }
+        WifiManager manager = (WifiManager) context.getSystemService(WIFI_SERVICE);
+
+        int wifiState = manager.getWifiState();
+        if ((wifiState == WifiManager.WIFI_STATE_ENABLING) || (wifiState == WifiManager.WIFI_STATE_ENABLED)) {
+            manager.setWifiEnabled(false);
+        }
+
+        if ((wifiState == WifiManager.WIFI_STATE_DISABLED)) {
+            manager.startLocalOnlyHotspot(new WifiManager.LocalOnlyHotspotCallback() {
+
+                @Override
+                public void onStarted(WifiManager.LocalOnlyHotspotReservation reservation) {
+                    super.onStarted(reservation);
+                    mReservation = reservation;
+                    SSID = reservation.getWifiConfiguration().SSID;
+                    preShareKey = reservation.getWifiConfiguration().preSharedKey;
+                    isHotspotEnabledState = true;
+                }
+
+                @Override
+                public void onStopped() {
+                    super.onStopped();
+                    isHotspotEnabledState = false;
+                }
+
+                @Override
+                public void onFailed(int reason) {
+                    super.onFailed(reason);
+                    isHotspotEnabledState = false;
+                }
+            }, new Handler());
+        }
+    }
+
+    public void turnOffHotspot() {
+//        if (!isLocationPermissionEnable(context)) {
+//            return;
+//        }
+        if (isHotspotEnabledState == true && mReservation != null) {
+            mReservation.close();
+            isHotspotEnabledState = false;
+        }
+    }
+
+    public ArrayList<String> getConnectedIP(){
+        ArrayList<String> connectedIp=new ArrayList<String>();
+        try {
+            BufferedReader br=new BufferedReader(new FileReader(
+                    "/proc/net/arp"));
+            String line;
+            while ((line=br.readLine())!=null){
+                String[] splitted=line.split(" +");
+                if (splitted !=null && splitted.length>=4){
+                    String ip=splitted[0];
+                    if (!ip.equalsIgnoreCase("ip")){
+                        connectedIp.add(ip);
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return connectedIp;
+    }
+
+    /**
+     * open the mobile network
+     * @param context
+     * @param enabled  open or closed
+     */
+    public void setMobileDataState(Context context, boolean enabled) {
+        TelephonyManager telephonyService = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        try {
+            Method setDataEnabled = telephonyService.getClass().getDeclaredMethod("setDataEnabled",boolean.class);
+            if (null != setDataEnabled) {
+                setDataEnabled.invoke(telephonyService, enabled);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     /**
      * @param on Boolean status
      */
-    public  boolean enableMobileData( boolean on) {
+    public boolean enableMobileData(boolean on) {
         try {
             ConnectivityManager mConnectivityManager =
                     (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -79,194 +189,21 @@ public class WifiHotspotConfig {
         return false;
     }
 
-
-    /**
-     * @param
-     * @param name     Name of hotspot
-     * @param password Password of hotspot
-     * @return
-     */
-    public  boolean configureHotspot(String name, String password) {
-        WifiManager manager = (WifiManager) context.getSystemService(WIFI_SERVICE);
-        WifiConfiguration configuration = getCustomConfigs(manager, name, password);
-        return configure(manager, configuration);
-    }
-
-    /**
-     * @return
-     */
-    private  boolean configure(WifiManager manager, WifiConfiguration wifiConfig) {
+    public void closeHotSpot(){
+        ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        Field iConnMgrField;
         try {
-            Method setConfigMethod = manager.getClass().getMethod("setWifiApConfiguration", WifiConfiguration.class);
-            boolean status = (boolean) setConfigMethod.invoke(manager, wifiConfig);
-
-
-            if (status) {
-                Log.d("HOTSPOT", "热点已开启 SSID:" + wifiConfig.SSID + wifiConfig.preSharedKey);
-
-            } else {
-                Log.d("HOTSPOT", "创建热点失败");
-            }
-            return status;
-        } catch (IllegalArgumentException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            iConnMgrField = connManager.getClass().getDeclaredField("mService");
+            iConnMgrField.setAccessible(true);
+            Object iConnMgr = iConnMgrField.get(connManager);
+            Class<?> iConnMgrClass = Class.forName(iConnMgr.getClass().getName());
+            Method stopTethering = iConnMgrClass.getMethod("stopTethering", int.class);
+            stopTethering.invoke(iConnMgr, 0);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return false;
-    }
-
-
-    /**
-     * @param ssid Requested ssid.
-     * @param pass Request password.
-     * @return
-     */
-    private  WifiConfiguration getCustomConfigs(WifiManager manager, String ssid, String pass) {
-        WifiConfiguration wifiConfig = null;
-        try {
-            Method getConfigMethod = manager.getClass().getMethod("getWifiApConfiguration");
-            wifiConfig = (WifiConfiguration) getConfigMethod.invoke(manager);
-            SSID = wifiConfig.SSID;
-            preShareKey = wifiConfig.preSharedKey;
-
-            if (!TextUtils.isEmpty(ssid))
-                wifiConfig.SSID = ssid;
-            if (!TextUtils.isEmpty(pass))
-                wifiConfig.preSharedKey = pass;
-            return wifiConfig;
-        } catch (IllegalArgumentException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        if (wifiConfig == null) {
-            wifiConfig = new WifiConfiguration();
-            if (!TextUtils.isEmpty(ssid))
-                wifiConfig.SSID = ssid;
-            if (!TextUtils.isEmpty(pass))
-                wifiConfig.preSharedKey = pass;
-            wifiConfig.hiddenSSID = true;
-            wifiConfig.allowedAuthAlgorithms.set(WifiConfiguration.AuthAlgorithm.OPEN);//开放系统认证
-            wifiConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
-            wifiConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
-            wifiConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
-            wifiConfig.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
-            wifiConfig.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
-            wifiConfig.status = WifiConfiguration.Status.ENABLED;
-        }
-        return wifiConfig;
-    }
-
-    public  String getHotspotSSID(Context context) {
-        WifiConfiguration wifiConfig = null;
-        try {
-            WifiManager manager = (WifiManager) context.getSystemService(WIFI_SERVICE);
-            Method getConfigMethod = manager.getClass().getDeclaredMethod("getWifiApConfiguration");
-
-            getConfigMethod.setAccessible(true);
-
-            wifiConfig = (WifiConfiguration) getConfigMethod.invoke(manager);
-            return wifiConfig.SSID;
-        }
-        catch (IllegalArgumentException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "abcd";
-    }
-
-    private  boolean isLocationPermissionEnable(Activity activity) {
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(activity, new String[] {Manifest.permission.ACCESS_COARSE_LOCATION}, 2);
-            return false;
-        }
-        return true;
-    }
-    private static WifiManager.LocalOnlyHotspotReservation mReservation;
-    private static boolean isHotspotEnabled = false;
-
-    /**
-     * description:turnOnHotspot() just start the hotspot
-     * and can not setup the SSID and Password
-     * @param
-     * @param
-     * @return
-     */
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    public  void turnOnHotspot() {
-        if (!isLocationPermissionEnable(activity)) {
-            return;
-        }
-        WifiManager manager = (WifiManager) context.getSystemService(WIFI_SERVICE);
-
-        int wifiState = manager.getWifiState();
-        if ((wifiState == WifiManager.WIFI_STATE_ENABLING)||(wifiState == WifiManager.WIFI_STATE_ENABLED)) {
-            manager.setWifiEnabled(false);
-        }
-
-        if (manager != null && (wifiState == WifiManager.WIFI_STATE_DISABLED|| wifiState==WifiManager.WIFI_STATE_DISABLING)) {
-            manager.startLocalOnlyHotspot(new WifiManager.LocalOnlyHotspotCallback() {
-
-                @Override
-                public void onStarted(WifiManager.LocalOnlyHotspotReservation reservation) {
-                    super.onStarted(reservation);
-                    mReservation = reservation;
-                    isHotspotEnabled = true;
-                    SSID = reservation.getWifiConfiguration().SSID;
-                    preShareKey = reservation.getWifiConfiguration().preSharedKey;
-                }
-
-                @Override
-                public void onStopped() {
-                    super.onStopped();
-                    Log.d("Hotspot", "onStopped: ");
-                    isHotspotEnabled = false;
-                }
-
-                @Override
-                public void onFailed(int reason) {
-                    super.onFailed(reason);
-                    Log.d("Hotspot", "onFailed: ");
-                    isHotspotEnabled = false;
-                }
-            }, new Handler());
-        }
-    }
-
-
-    /**
-     * 获取SSID
-     * @param activity 上下文
-     * @return  WIFI 的SSID
-     */
-    public String getWIFISSID(Activity activity) {
-        String ssid="unknown id";
-
-        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.O||Build.VERSION.SDK_INT==Build.VERSION_CODES.P) {
-
-            WifiManager mWifiManager = (WifiManager) activity.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
-
-            assert mWifiManager != null;
-            WifiInfo info = mWifiManager.getConnectionInfo();
-
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
-                return info.getSSID();
-            } else {
-                return info.getSSID().replace("\"", "");
-            }
-        } else if (Build.VERSION.SDK_INT==Build.VERSION_CODES.O_MR1){
-
-            ConnectivityManager connManager = (ConnectivityManager) activity.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-            assert connManager != null;
-            NetworkInfo networkInfo = connManager.getActiveNetworkInfo();
-            if (networkInfo.isConnected()) {
-                if (networkInfo.getExtraInfo()!=null){
-                    return networkInfo.getExtraInfo().replace("\"","");
-                }
-            }
-        }
-        return ssid;
     }
 
 }
+
+
